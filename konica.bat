194751b4d8fd7a4c7774bbac92f14fb43cde69b1
CodeRSaldivar <125710128+CodeRSaldivar@users.noreply.github.com>
2023-05-19 10:20:40 -0500

    Update konica.bat

--- konica.bat
+++ konica.bat
@@ -1,7 +1,36 @@
-:: D:\C750i_C650i_C360i_C287i_C286i_C4050i_C4000i_Series_C3320i_PS_PCL_FAX_v3.1.12.0\Drivers\PCL\EN\Win_x64
+mkdir c:\Users\Public\.temp\C550i
 
-::KONICA MINOLTA C650iSeries PCL
-
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/KOAXCJ__.inf' -OutFile 'c:\Users\Public\.temp\C550i\KOAXCJ__.inf'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/KOAXCJ__.unf' -OutFile 'c:\Users\Public\.temp\C550i\KOAXCJ__.unf'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj__.cat' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj__.cat'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj__.ch_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj__.ch_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj__.kp_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj__.kp_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_a.km_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_a.km_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_b.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_b.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_b.km_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_b.km_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_c.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_c.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_c.km_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_c.km_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_c.kp_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_c.kp_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_d.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_d.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_d.km_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_d.km_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_d.kp_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_d.kp_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_e.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_e.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_f.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_f.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_g.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_g.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_j.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_j.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_l.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_l.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_m.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_m.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_m.kp_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_m.kp_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_n.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_n.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_o.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_o.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_r.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_r.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_s.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_s.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_u.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_u.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_w.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_w.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/koaxcj_x.dl_' -OutFile 'c:\Users\Public\.temp\C550i\koaxcj_x.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/kobdrvapiif.dl_' -OutFile 'c:\Users\Public\.temp\C550i\kobdrvapiif.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/kobdrvapiif32.dl_' -OutFile 'c:\Users\Public\.temp\C550i\kobdrvapiif32.dl_'"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/kobdrvapiw64.ex_' -OutFile 'c:\Users\Public\.temp\C550i\kobdrvapiw64.ex_'"
 
 C:\Windows\System32\Cscript.exe C:\Windows\System32\Printing_Admin_Scripts\en-US\Prnport.vbs -a -r KONICA-ip-Port -h 10.102.216.230 -o lpr -n 515 -q print -y public -i 1
 
@@ -14,8 +43,14 @@ C:\Windows\System32\Cscript.exe C:\Windows\System32\Printing_Admin_Scripts\en-US
 
 C:\Windows\System32\rundll32.exe printui.dll,PrintUIEntry /y /n "103 (SGW) KONICA MINOLTA bizhub C550i"
 
-echo.
 
 echo.
+
+
+echo.
+
 
 echo Done!!!
+
+
+:: Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/konica.bat' -OutFile '103-SGW-KONICA.bat'
