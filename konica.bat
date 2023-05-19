c55fac88509d2f22cc14d8c6c2cc5577e49f75e7
CodeRSaldivar <125710128+CodeRSaldivar@users.noreply.github.com>
2023-05-19 10:29:59 -0500

    Update konica.bat

--- konica.bat
+++ konica.bat
@@ -53,4 +53,4 @@ echo.
 echo Done!!!
 
 
-:: Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/konica.bat' -OutFile '103-SGW-KONICA.bat'
+:: C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CodeRSaldivar/103-SGW-KONICA/main/konica.bat' -OutFile '.\103-SGW-KONICA.bat'"
